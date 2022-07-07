52b37d123c6a8189306645baffdfc37d36af70e2
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-07 16:01:26 -0600

    initial answers

--- functions.js
+++ functions.js
@@ -1,9 +1,35 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(value1, value2) {
+  return value1 + value2
+
 }
+
+
+function sayHello(name) {
+  if (name === 'Will') {
+  return 'No more testing Will!'
+  } else {
+return `Hi there ${name}!`
+   }
+  }
+
+
+
+function buildCar(color, type) {
+if (color === undefined && type === undefined){
+  return {}
+  }
+else {
+  return {
+    color: color,
+    type: type,
+  }
+}
+}
+
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
